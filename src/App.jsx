a809048ae3ee4d1bde7e2a3adda68d31a5440c2a
Vinaykrish25 <vinaykrish2002@gmail.com>
2025-09-08 14:44:18 +0530

    Task Completed with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ const App = () => {
        <BrowserRouter>
        <Routes>
         <Route path='/' element={<Homepage />}/>
-        <Route path='products' element={<ProductDetailPage />}/>
+        <Route path='/products' element={<ProductDetailPage />}/>
         <Route path='/category' element={<CategoryPage />}/>
         <Route path='/filter' element={<Filter />}/>
-        <Route path='cart' element={<Cart />}/>
+        <Route path='/cart' element={<Cart />}/>
        </Routes>
        </BrowserRouter>
     </div>
